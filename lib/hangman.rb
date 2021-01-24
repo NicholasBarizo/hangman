6d29d2bfaf39d38b1af98546fb65c7e0ab3cee89
Nicholas <nicholasbarizo@gmail.com>
2021-01-23 23:59:35 -0800

    shows amount of incorrect guesses and amount of times a letter shows in the wodr

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -6,14 +6,21 @@ class Hangman
     puts secret_word
     lives = 6
     blanks = '_' * secret_word.length
+    wrong_guesses = ''
     while lives.positive?
       puts "You have #{lives} chance#{'s' if lives > 1} remaining"
-      guess = get_guess
+      guess = get_guess(wrong_guesses)
       if secret_word.split('').any? { |char| char == guess }
-        blanks = game_turn(secret_word, blanks, guess)
+        puts "\n\n#{guess} appears #{secret_word.count(guess)} time#{'s' if secret_word.count(guess) > 1} in the word"
+        turn_values = game_turn(secret_word, blanks, guess, wrong_guesses)
+        blanks = turn_values[0]
+        wrong_guesses = turn_values[1]
         end_game('won', lives, secret_word) if blanks == secret_word
       else
+        puts "\n\n#{guess} does not appear in the word"
         lives -= 1
+        wrong_guesses += guess
+        puts "\n#{blanks.split('').join(' ')}\n "
       end
     end
     end_game('loss', lives, secret_word)
@@ -31,40 +38,48 @@ class Hangman
     word.downcase
   end
 
-  def game_turn(secret_word, blanks, guess)
-    blanks = compare_words(secret_word, guess, blanks)
+  def game_turn(secret_word, blanks, guess, wrong_guesses)
+    comparison_outcome = compare_words(secret_word, guess, blanks, wrong_guesses)
+    blanks = comparison_outcome[0]
+    wrong_guesses = comparison_outcome[1]
     puts "\n#{blanks.split('').join(' ')}\n "
-    blanks
+    [blanks, wrong_guesses]
   end
 
-  def get_guess()
+  def get_guess(wrong_guesses)
+    puts "Incorrect guesses: #{wrong_guesses.split('').join(' ')}" unless wrong_guesses.empty?
     puts 'Guess a letter'
     guess = gets.chomp.downcase
-    until guess.length == 1 && guess.split('').all? { |char| ('a'..'z').include? char}
-      require_valid_word(guess)
+    until guess.length == 1 && ('a'..'z').include?(guess) && !wrong_guesses.include?(guess)
+      require_valid_word(guess, wrong_guesses)
       guess = gets.chomp.downcase
     end
     guess
   end
 
-  def require_valid_word(guess)
+  def require_valid_word(guess, wrong_guesses)
     puts 'Guess a letter' unless guess.length == 1
-    puts 'The word only contains letters' unless guess.split('').all? { |char| ('a'..'z').include? char}
+    puts 'The word only contains letters' unless guess.split('').all? { |char| ('a'..'z').include? char }
+    puts "You already guessed #{guess} incorrectly" unless wrong_guesses.include?(guess)
   end
 
-  def compare_words(secret_word, guess, blanks)
+  def compare_words(secret_word, guess, blanks, wrong_guesses)
     modified_blanks = blanks
     blanks.split('').each_with_index do |_, blanks_index|
       secret_word.split('').each_with_index do |secret_char, secret_index|
-        if secret_char == guess && blanks_index == secret_index
+        if secret_char == guess && blanks_index == secret_index && !wrong_guesses.include?(guess)
           modified_blanks = modified_blanks.split('').map.with_index do |modify_char, modify_index|
             modify_index == secret_index ? guess : modify_char
           end
           modified_blanks = modified_blanks.join('')
+        # elsif secret_word.split('').none? { |char| char == guess }
+        #   # blanks_index == secret_index && !wrong_guesses.include?(guess)
+        #   puts "#{blanks_index} #{secret_index} "
+        #   wrong_guesses += guess
         end
       end
     end
-    modified_blanks
+    [modified_blanks, wrong_guesses]
   end
 
   def end_game(outcome, lives, word)
